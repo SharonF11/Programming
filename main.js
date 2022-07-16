95cf3ae0fc2f7f666797d35ab4d41e39bda22b99
SharonF11 <89339561+SharonF11@users.noreply.github.com>
2022-07-16 14:16:58 +0100

    sports website

---
+++ main.js
@@ -0,0 +1,24 @@
+/*This function allows the collapsible on the navbar menu when clicked- The JS was set up bye Sharon Farrell  */
+const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach((item) =>
+  item.addEventListener("click", function () {
+    this.classList.toggle("collapsible--expanded");
+  })
+);
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+// This code was taken from W3Schools for a dropdown button to show items 
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
